71783dc50853368a5088d48d003e230c0ee313e3
371931 <issacsamuelraj321@gmail.com>
2025-03-01 19:10:49 +0530

    .

---
+++ src/pages/auth/components/AuthenticationForm/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./AuthenticationForm.module.scss";
+
+type Props = {};
+
+const AuthenticationForm: React.FC<Props> = () => {
+  return <div></div>;
+};
+
+export default React.memo(AuthenticationForm);
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const AuthPage: React.FC<Props> = ({}) => {
+  return <div></div>;
+};
+
+export default React.memo(AuthPage);
